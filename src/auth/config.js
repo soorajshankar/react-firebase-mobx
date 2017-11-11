6f8e084faab8d26656a40096e6ce2667ee37bf79
Sooraj Shankar <soorajshankar@gmail.com>
2017-11-12 01:22:34 +0530

    Minor Changes

--- src/auth/config.js
+++ src/auth/config.js
@@ -1,17 +1,9 @@
 import firebase from 'firebase'
 
-// const config = {
-//   apiKey: "AIzaSyDHL6JFTyBcaV60WpE4yXfeO0aZbzA9Xbk",
-//   authDomain: "practice-auth.firebaseapp.com",
-//   databaseURL: "https://practice-auth.firebaseio.com",
-// }
 const config = {
-  apiKey: "AIzaSyCtZN8ZFq-OeU9Q0mBYBqokaeVDA3WIoQw",
-  authDomain: "fir-test-a4e00.firebaseapp.com",
-  databaseURL: "https://fir-test-a4e00.firebaseio.com",
-  projectId: "fir-test-a4e00",
-  storageBucket: "fir-test-a4e00.appspot.com",
-  messagingSenderId: "54905034528"
+  apiKey: "AIzaSyDHL6JFTyBcaV60WpE4yXfeO0aZbzA9Xbk",
+  authDomain: "practice-auth.firebaseapp.com",
+  databaseURL: "https://practice-auth.firebaseio.com",
 }
 firebase.initializeApp(config)
 
